1aa00fea87af268b5760cbdcda2d3e6943af380c
Gabor Szabo <gabor@szabgab.com>
2023-08-15 10:26:03 +0200

    first working version of status.rs

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 A command line tool to write status messages that will appear on my web site and/or it might set the status on some of my accounts (e.g. What's App status.
 
-It is sort of a replacement for twitter.
+It will help me learn where I spend my time.
 
-Or maybe it will help me learn where I spend my time.
+
+* Get the current date
+* Save message with the corrent datae in a file in the home directory of the user ~/.status-rs/messages.json
+
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::process::exit;
+use std::fs::File;
+use std::io::Write;
+
 use clap::Parser;
+use chrono::{Utc, DateTime};
+use chrono::serde::ts_seconds;
+use serde::{Serialize};
 
 #[derive(Parser, Debug)]
 #[command(version)]
@@ -7,8 +14,39 @@ struct Cli {
     message: String,
 }
 
+#[derive(Debug, Serialize)]
+#[allow(dead_code)]
+struct Message {
+    text: String,
+
+    #[serde(with = "ts_seconds")]
+    now: DateTime<Utc>,
+}
+
 
 fn main() {
     let args = Cli::parse();
-    dbg!(&args);
+    //dbg!(&args);
+    if args.message == "" {
+        eprintln!("-m message    is required");
+        exit(1);
+    }
+
+    let msg = Message {
+        text: args.message,
+        now: Utc::now(),
+    };
+
+    let serialized = serde_json::to_string(&msg).unwrap();
+    //println!("{}", serialized);
+
+    let filename = "messages.json";
+    let mut fh = match File::options().append(true).open(filename) {
+        Ok(fh) => fh,
+        Err(_) => {
+            File::create(filename).unwrap()
+        }
+    };
+    writeln!(&mut fh, "{}", serialized).unwrap();
+
 }
